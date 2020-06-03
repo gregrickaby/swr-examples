665a78dafee162376f4695b9c32a1dfdb1f25093
Greg Rickaby <greg@gregrickaby.com>
2020-06-03 17:11:09 -0500

    level up the header

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,19 @@ import Head from 'next/head'
 const Header = () => (
   <Head>
     <title>SWR Examples</title>
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
+    <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+    <meta name="description" content="View images from a subreddit." />
+    <link rel="dns-prefetch" href="//www.reddit.com" />
+    <link
+      rel="preload"
+      href="//www.reddit.com/r/itookapicture/.json?limit=200&show=all"
+      as="fetch"
+      crossOrigin="anonymous"
+    />
     <link rel="icon" href="/favicon.ico" />
   </Head>
 )
