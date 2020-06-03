8e731ade935f2b0b913c98aab2676b91aa47f6cb
Greg Rickaby <greg@gregrickaby.com>
2020-06-03 10:18:50 -0500

    add codesandbox links

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # SWR Examples
 
 [SWR](https://swr.now.sh/) is a React Hooks library for remote data fetching. https://swr-examples.now.sh
+
+[![Edit gregrickaby/swr-examples](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/github/gregrickaby/swr-examples/tree/master/?autoresize=1&fontsize=14&hidenavigation=1&theme=dark)
--- pages/basic-example.js
+++ pages/basic-example.js
@@ -4,29 +4,40 @@ const fetcher = (url) => fetch(url).then((r) => r.json());
 
 export default function IndexPage() {
   const { data, error } = useSWR(
-    `https://www.reddit.com/r/itookapicture/.json?limit=5&show=all`,
+    `https://www.reddit.com/r/itookapicture/.json?limit=3&show=all`,
     fetcher
   );
   if (error) return <div>Failed to load</div>;
   if (!data) return <div>loading subreddit...</div>;
   return (
     <>
-      <h1>Reddit Viewer</h1>
-      💻{" "}
-      <a href="https://github.com/gregrickaby/swr-examples/blob/master/pages/basic-example.js">
-        View source code on Github
+      <h1>Basic Example</h1>
+      <a href="https://codesandbox.io/s/github/gregrickaby/swr-examples/tree/master/?autoresize=1&fontsize=14&hidenavigation=1&initialpath=%2Fbasic-example&module=%2Fpages%2Fbasic-example.js&moduleview=1&theme=dark">
+        <img
+          alt="Edit gregrickaby/swr-examples"
+          src="https://codesandbox.io/static/img/play-codesandbox.svg"
+        />
       </a>
-      {data.data.children.map((post, index) => (
-        <div key={index}>
-          <h2>
-            <a
-              href={`https://www.reddit.com${post.data.permalink}`}
-              dangerouslySetInnerHTML={{ __html: post.data.title }}
-            />
-          </h2>
-          <img src={post.data.thumbnail} alt={post.data.title} />
-        </div>
-      ))}
+      <div className="example-content">
+        {data.data.children.map((post, index) => (
+          <div key={index}>
+            <h2>
+              <a
+                href={`https://www.reddit.com${post.data.permalink}`}
+                dangerouslySetInnerHTML={{ __html: post.data.title }}
+              />
+            </h2>
+            <img src={post.data.thumbnail} alt={post.data.title} />
+          </div>
+        ))}
+      </div>
+      <style jsx>
+        {`
+          .example-content {
+            border-top: 1px solid #000;
+          }
+        `}
+      </style>
     </>
   );
 }
