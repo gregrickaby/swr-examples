2b282232c62f54540836c736b109f89540f7868b
Greg Rickaby <greg@gregrickaby.com>
2020-06-03 17:08:20 -0500

    add header/footer components

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+const Header = () => (
+  <Head>
+    <title>SWR Examples</title>
+    <link rel="icon" href="/favicon.ico" />
+  </Head>
+)
+
+export default Header
