73039ada7e66f41d1ad002d28814772d3e320cf3
Greg Rickaby <greg@gregrickaby.com>
2020-06-04 16:47:16 -0500

    use SWAPI

--- example-basic/pages/index.js
+++ example-basic/pages/index.js
@@ -3,10 +3,7 @@ import useSWR from "swr";
 const fetcher = (url) => fetch(url).then((r) => r.json());
 
 const ExampleBasic = () => {
-  const { data, error } = useSWR(
-    `https://www.reddit.com/r/itookapicture/.json?limit=2&show=all`,
-    fetcher
-  );
+  const { data, error } = useSWR(`https://swapi.dev/api/people/1/`, fetcher);
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
   return <pre>{JSON.stringify(data, null, 2)}</pre>;
