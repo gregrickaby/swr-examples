577bc5573285278ec3a90571abb76213c78519d7
Greg Rickaby <greg@gregrickaby.com>
2020-06-04 16:48:38 -0500

    add linebreak

--- example-basic/pages/index.js
+++ example-basic/pages/index.js
@@ -4,6 +4,7 @@ const fetcher = (url) => fetch(url).then((r) => r.json());
 
 const ExampleBasic = () => {
   const { data, error } = useSWR(`https://swapi.dev/api/people/1/`, fetcher);
+
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
   return <pre>{JSON.stringify(data, null, 2)}</pre>;
