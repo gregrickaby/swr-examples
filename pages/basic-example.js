988cda96e81ea1333f8ad8068a84fd10472205ff
Greg Rickaby <greg@gregrickaby.com>
2020-06-03 21:04:38 -0500

    fix it up

--- pages/basic-example.js
+++ pages/basic-example.js
@@ -1,10 +1,11 @@
 import useSWR from 'swr'
+import Link from 'next/link'
 
 const fetcher = (url) => fetch(url).then((r) => r.json()) // eslint-disable-line no-undef
 
-export default function IndexPage() {
+const BasicExample = () => {
   const {data, error} = useSWR(
-    `https://www.reddit.com/r/itookapicture/.json?limit=3&show=all`,
+    `https://www.reddit.com/r/itookapicture/.json?limit=8&show=all`,
     fetcher
   )
   if (error) return <div>Failed to load</div>
@@ -12,18 +13,27 @@ export default function IndexPage() {
   return (
     <>
       <div className="example-content">
-        {data.data.children.map((post, index) => (
-          <div key={index}>
-            <h2>
-              <a
-                href={`https://www.reddit.com${post.data.permalink}`}
-                dangerouslySetInnerHTML={{__html: post.data.title}}
-              />
-            </h2>
-            <img src={post.data.thumbnail} alt={post.data.title} />
-          </div>
-        ))}
+        <h1 className="text-2xl my-8 text-center">Basic Example</h1>
+        <p className="text-center">Display posts from r/itookapicture</p>
+        <div className="grid grid-cols-2 gap-4">
+          {data.data.children.map((post, index) => (
+            <div key={index}>
+              <h2>
+                <a
+                  href={`https://www.reddit.com${post.data.permalink}`}
+                  dangerouslySetInnerHTML={{__html: post.data.title}}
+                />
+              </h2>
+              <img src={post.data.thumbnail} alt={post.data.title} />
+            </div>
+          ))}
+        </div>
       </div>
+      <Link href="/">
+        <a className="mt-8 text-center">Back home</a>
+      </Link>
     </>
   )
 }
+
+export default BasicExample
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,40 @@
 import SiteHead from '@/components/SiteHead'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
+import Link from 'next/link'
 
 const Home = () => (
   <>
     <SiteHead />
     <Header />
     <main className="container">
-      <div className="example">
-        <h2 className="text-2xl mb-2">Basic Example</h2>
-        <iframe
-          src="https://codesandbox.io/embed/github/gregrickaby/swr-examples/tree/master/?autoresize=1&fontsize=14&hidenavigation=1&initialpath=%2Fbasic-example&module=%2Fpages%2Fbasic-example.js&moduleview=1&theme=dark"
-          className="iframe"
-          title="gregrickaby/swr-examples"
-          allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
-          sandbox="allow-autoplay allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-        />
+      <div className="flex flex-col justify-center items-center text-center">
+        <Link href="/basic-example">
+          <a className="underline">
+            <h2 className="text-2xl mb-2">Basic Example</h2>
+          </a>
+        </Link>
+        <pre className="text-left whitespace-pre overflow-x-auto">
+          {`
+          import useSWR from 'swr'
+
+          const fetcher = (url) => fetch(url).then((r) => r.json())
+
+          function Profile() {
+            const { data, error } = useSWR('/api/user', fetcher)
+
+            if (error) return <div>failed to load</div>
+            if (!data) return <div>loading...</div>
+            return <div>hello {data.name}!</div>
+          }
+          `}
+        </pre>
+        <a href="https://codesandbox.io/s/github/gregrickaby/swr-examples/tree/master/?autoresize=1&fontsize=14&hidenavigation=1&initialpath=%2Fbasic-example&moduleview=1&theme=dark">
+          <img
+            alt="Edit gregrickaby/swr-examples"
+            src="https://codesandbox.io/static/img/play-codesandbox.svg"
+          />
+        </a>
       </div>
     </main>
     <Footer />
