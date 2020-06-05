bd9c6bc7234337cc6f7d8984df5b98d882f95c45
Greg Rickaby <greg@gregrickaby.com>
2020-06-05 17:00:34 -0500

    add suspense example

---
+++ example-react-suspense/pages/index.js
@@ -0,0 +1,19 @@
+import { Suspense } from "react";
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((r) => r.json());
+
+const Profile = () => {
+  const { data } = useSWR("https://swapi.dev/api/people/1/", fetcher, {
+    suspense: true,
+  });
+  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+};
+
+const Example = () => (
+  <Suspense fallback={<div>loading...</div>}>
+    <Profile />
+  </Suspense>
+);
+
+export default Example;
