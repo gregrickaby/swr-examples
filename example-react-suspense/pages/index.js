87a404baf4585f114b7635b4561e07d7d2b48a87
Greg Rickaby <greg@gregrickaby.com>
2020-06-05 17:32:43 -0500

    bump

--- example-react-suspense/pages/index.js
+++ example-react-suspense/pages/index.js
@@ -4,7 +4,7 @@ import useSWR from "swr";
 const fetcher = (url) => fetch(url).then((r) => r.json());
 
 const Profile = () => {
-  const { data } = useSWR("https://swapi.dev/api/people/1/", fetcher, {
+  const { data } = useSWR(`https://swapi.dev/api/people/1/`, fetcher, {
     suspense: true,
   });
   return <pre>{JSON.stringify(data, null, 2)}</pre>;
